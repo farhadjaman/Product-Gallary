a1c9eb1bdb712ec3d694e5c8c1efa8893bb73223
MD Farhad Jaman <dev@growthandanalytics.com>
2023-11-05 02:30:59 +0600

    Added custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        darkerGrotesque: ['Darker Grotesque', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          100: '#E4F6FF',
+          200: '#01CC96',
+        },
+        secondary: '#F5F7FB',
+      },
+    },
   },
   plugins: [],
 };
