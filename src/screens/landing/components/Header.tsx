9f6af17190dd591cef6ba6ae84e2c107fd010318
MD Farhad Jaman <dev@growthandanalytics.com>
2023-11-05 09:35:20 +0600

    feat(Gallary): Added responsive UI components

--- src/screens/landing/components/Header.tsx
+++ src/screens/landing/components/Header.tsx
@@ -3,12 +3,14 @@ import SelectIcon from 'assets/icons/Select.icon';
 
 const Headers = () => {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col md:flex-row justify-between">
       <div className="flex flex-col">
         <div className=" text-[36px] font-darkerGrotesque font-bold">Accessories</div>
-        <p className="text-[20px]">Essentials that pairs perfectly with your favourite device</p>
+        <p className="text-sm md:text-xl">
+          Essentials that pairs perfectly with your favourite device
+        </p>
       </div>
-      <div className="flex gap-4 pt-4">
+      <div className="flex gap-4 pt-4 justify-between">
         <div className="flex gap-2 align-middle justify-center">
           <SelectIcon width={20} height={25} />
           <p className="font-inter font-semibold text-md">3 Items Selected</p>
--- src/screens/landing/index.tsx
+++ src/screens/landing/index.tsx
@@ -5,7 +5,7 @@ import ImageIcon from 'assets/icons/Image.icon';
 
 const index = () => {
   return (
-    <div className="font-inter px-20 py-4 py flex flex-col gap-8 mb-12">
+    <div className="font-inter px-8 md:px-16 lg:px-20 py-4 py flex flex-col gap-8 mb-12">
       <Header />
       <div className="w-full h-full grid  grid-cols-4 md:grid-cols-6 lg:grid-cols-12 grid-rows-3 gap-y-12 gap-x-8">
         {items.map((item, index) => (
@@ -13,7 +13,7 @@ const index = () => {
         ))}
         <div className="aspect-square col-span-2 flex flex-col gap-4 bg-secondary rounded-[24px] items-center justify-center">
           <ImageIcon />
-          <div className="col-span-2 text-center px-6 py-2 bg-primary-200 text-white text-lg font-bold rounded-3xl">
+          <div className="col-span-2 text-center px-6 py-2 bg-primary-200 text-white text-sm md:text-lg font-bold rounded-2xl">
             Add More
           </div>
         </div>
